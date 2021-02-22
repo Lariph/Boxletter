f62bb6d54ec9f4604a462587806c0c96a4875166
Lariph <larissa.pires98@gmail.com>
2021-02-22 13:02:42 -0300

    navbar corrigida

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app >
+
     <v-app-bar  
-      absolute dark
+      dark app
       color="orange"      
       elevate-on-scroll
       scroll-target="#scrolling-techniques-7">
@@ -9,13 +10,15 @@
         <v-toolbar-title>Boxletter</v-toolbar-title>
     </v-app-bar>
 
-    <router-view></router-view>
+    <v-content>
+      <router-view ></router-view>
+    </v-content>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-list-item-group v-model="group" active-class="deep-orange--text text--accent-4">
           
-          <v-list-item>
+          <v-list-item href='http://localhost:8080/'>
             <v-list-item-icon>
               <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
@@ -46,6 +49,7 @@
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
+  
   </v-app>
 </template>
 
